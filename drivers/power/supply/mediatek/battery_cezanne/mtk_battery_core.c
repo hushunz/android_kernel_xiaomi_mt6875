ad36550dc534eecdfc7ec0dc5b95c1301d9d78d2
hushunz <149858221+hushunz@users.noreply.github.com>
2026-09-25 11:19:45 +0800

    Refactor battery resistance handling in mtk_battery_core

--- drivers/power/supply/mediatek/battery_cezanne/mtk_battery_core.c
+++ drivers/power/supply/mediatek/battery_cezanne/mtk_battery_core.c
@@ -880,8 +880,8 @@ void fg_custom_init_from_header(void)
 			g_SHUTDOWN_HL_ZCV[i][gm.battery_id];
 
 		for (j = 0; j < 100; j++)
-			if (p[j].resistance2 == 0)
-				p[j].resistance2 = p[j].resistance;
+		      if (p[j].charge_r.rdc[0] == 0)
+				p[j].charge_r.rdc[0] = p[j].resistance;
 
 	}
 
@@ -944,7 +944,9 @@ static void fg_custom_parse_table(const struct device_node *np,
 		const char *node_srting,
 		struct FUELGAUGE_PROFILE_STRUCT *profile_struct, int column)
 {
-	int mah, voltage, resistance, idx, saddles, resistance2;
+	int mah, voltage, resistance, idx, saddles;
+	int i = 0, charge_rdc[MAX_CHARGE_RDC];
+	
 	struct FUELGAUGE_PROFILE_STRUCT *profile_p;
 
 	profile_p = profile_struct;
@@ -966,20 +968,38 @@ static void fg_custom_parse_table(const struct device_node *np,
 				np, node_srting, idx, &resistance)) {
 		}
 		idx++;
-		if (column == 4) {
+		
+		
+		
+		if (column == 3) {
+			for (i = 0; i < MAX_CHARGE_RDC; i++)
+				charge_rdc[i] = resistance;
+		} else if (column >= 4) {
 			if (!of_property_read_u32_index(
-				np, node_srting, idx, &resistance2))
+				np, node_srting, idx, &charge_rdc[0]))
 				idx++;
-		} else
-			resistance2 = resistance;
+		}
 
-		bm_debug("%s: mah: %d, voltage: %d, resistance: %d, resistance2: %d\n",
-			__func__, mah, voltage, resistance, resistance2);
+		/* read more for column >4 case */
+		if (column > 4) {
+			for (i = 1; i <= column - 4; i++) {
+				if (!of_property_read_u32_index(
+					np, node_srting, idx, &charge_rdc[i]))
+					idx++;
+			}
+		}
+
+		bm_debug("%s: mah: %d, voltage: %d, resistance: %d, rdc0:%d rdc:%d %d %d %d\n",
+			__func__, mah, voltage, resistance, charge_rdc[0],
+			charge_rdc[1], charge_rdc[2], charge_rdc[3], charge_rdc[4]);
 
 		profile_p->mah = mah;
 		profile_p->voltage = voltage;
 		profile_p->resistance = resistance;
-		profile_p->resistance2 = resistance2;
+		
+		for (i = 0; i < MAX_CHARGE_RDC; i++)
+			profile_p->charge_r.rdc[i] = charge_rdc[i];
+			
 
 		profile_p++;
 
@@ -999,7 +1019,10 @@ static void fg_custom_parse_table(const struct device_node *np,
 		profile_p->mah = mah;
 		profile_p->voltage = voltage;
 		profile_p->resistance = resistance;
-		profile_p->resistance2 = resistance2;
+		for (i = 0; i < MAX_CHARGE_RDC; i++)
+			profile_p->charge_r.rdc[i] = charge_rdc[i];
+
+
 		idx = idx + column;
 	}
 }
@@ -1412,8 +1435,8 @@ void fg_custom_init_from_dts(struct platform_device *dev)
 			i*TOTAL_BATTERY_NUMBER+gm.battery_id,
 			&(fg_table_cust_data.fg_profile[i].shutdown_hl_zcv), 1);
 		for (j = 0; j < 100; j++) {
-			if (p[j].resistance2 == 0)
-				p[j].resistance2 = p[j].resistance;
+			if (p[j].charge_r.rdc[0] == 0)
+				p[j].charge_r.rdc[0] = p[j].resistance;
 		}
 	}
 
@@ -2840,6 +2863,36 @@ void fg_daemon_comm_INT_data(char *rcv, char *ret)
 				&soc_setting, sizeof(soc_setting));
 		}
 		break;
+		case FG_GET_IS_FORCE_FULL:
+		{
+			/* 1 = trust customer full condition */
+			/* 0 = using gauge ori full flow */
+			int force_full = gm.is_force_full;
+
+			memcpy(&pret->output,
+				&force_full, sizeof(force_full));
+		}
+		break;
+	case FG_GET_ZCV_INTR_CURR:
+		{
+			int zcv_cur = 0;
+
+			gauge_get_zcv_current(&zcv_cur);
+			memcpy(&pret->output,
+				&zcv_cur, sizeof(zcv_cur));
+
+			bm_err("get FG_GET_ZCV_INTR_CURR %d\n", zcv_cur);
+		}
+		break;
+	case FG_GET_CHARGE_POWER_SEL:
+		{
+			int charge_power_sel = gm.charge_power_sel;
+
+			memcpy(&pret->output,
+				&charge_power_sel, sizeof(charge_power_sel));
+			bm_err("charge_power_sel = %d\n", charge_power_sel);
+		}
+		break;
 	case FG_SET_SOC:
 		{
 			gm.soc = (prcv->input + 50) / 100;
@@ -2959,6 +3012,44 @@ void bmd_ctrl_cmd_from_user(void *nl_data, struct fgd_nl_msg_t *ret_msg)
 	ret_msg->fgd_cmd = msg->fgd_cmd;
 
 	switch (msg->fgd_cmd) {
+	
+		/* A12/A13 追加的两个命令。daemon 每次算完容量都先发
+	 * SET_BATTERY_CAPACITY，紧接着才发 SET_KERNEL_UISOC/SOC（官核实测
+	 * 517.73s 就是这么一对）。A11 内核没有这两项时 daemon 判定
+	 * FG_DAEMON_CMD mismatch 并重启，永远走不到写 SOC 那一步。 */
+	case FG_DAEMON_CMD_SET_BATTERY_CAPACITY:
+		{
+			struct fgd_cmd_param_t_8 param;
+
+			memcpy(&param, &msg->fgd_data[0],
+				sizeof(struct fgd_cmd_param_t_8));
+
+			if (param.data[10] != 0 && param.data[11] != 0) {
+				gm.show_ag = param.data[10];
+				gm.bat_health = param.data[11];
+			}
+
+			gm.prev_batt_fcc = param.data[4];
+			gm.prev_batt_remaining_capacity =
+				param.data[4] / 10 * param.data[6] / 10000;
+			bm_debug(
+				"[fr] FG_DAEMON_CMD_SET_BATTERY_CAPACITY = %d %d %d %d %d %d %d %d %d %d RM:%d\n",
+				param.data[0], param.data[1], param.data[2],
+				param.data[3], param.data[4], param.data[5],
+				param.data[6], param.data[7], param.data[8],
+				param.data[9],
+				param.data[4] * param.data[6] / 10000);
+		}
+		break;
+
+	case FG_DAEMON_CMD_GET_BH_DATA:
+		{
+			ret_msg->fgd_data_len +=
+				sizeof(struct ag_center_data_st);
+			memcpy(ret_msg->fgd_data, &gm.bh_data,
+				sizeof(struct ag_center_data_st));
+		}
+		break;
 
 	case FG_DAEMON_CMD_IS_BAT_PLUGOUT:
 		{
@@ -4656,6 +4747,3 @@ void gm3_log_dump(bool force)
 	gm.log.phone_state = 0;
 
 }
-
-
-
